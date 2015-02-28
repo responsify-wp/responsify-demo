99fb10c516debc1b3e73d63260e8568aada25696
Stefan Ledin <stefan.ledin@gmail.com>
2015-02-28 23:11:42 +0100

    concat & minify

--- wp-content/themes/rwp_demo/includes/enqueue-scripts-styles.php
+++ wp-content/themes/rwp_demo/includes/enqueue-scripts-styles.php
@@ -6,16 +6,31 @@ function enqueue_scripts_and_styles () {
 	
 	// wp_enqueue_style( $handle, $src, $deps, $ver, $media );
 	wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css', null, null );
-	wp_enqueue_style( 'lazyloadxt', $assetsDir.'/css/jquery.lazyloadxt.fadein.min.css', null, null );
-	wp_enqueue_style( 'stylesheet', $assetsDir.'/css/style.css', null, filemtime($assetsPath.'/css/style.css'), null );
+	if ( strpos($_SERVER['SERVER_NAME'], 'responsify-demo.dev') ) {
+		if ( !empty($_GET['production']) ) {
+			wp_enqueue_style( 'stylesheet', $assetsDir.'/css/styles.min.css', null, filemtime($assetsPath.'/css/styles.min.css'), null );
+		} else {
+			wp_enqueue_style( 'lazyloadxt', $assetsDir.'/css/jquery.lazyloadxt.fadein.min.css', null, null );
+			wp_enqueue_style( 'stylesheet', $assetsDir.'/css/style.css', null, filemtime($assetsPath.'/css/style.css'), null );
+		}
+	} else {
+		wp_enqueue_style( 'stylesheet', $assetsDir.'/css/styles.min.css', null, filemtime($assetsPath.'/css/styles.min.css'), null );
+	}
+	
 	
 	// wp_enqueue_scripts($handle, $src, $deps, $ver, $in_footer);
 	wp_deregister_script( 'jquery' );
-	wp_register_script( 'jquery', '//code.jquery.com/jquery-latest.min.js', null, null, true );
-	wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'lazyloadxt', $assetsDir.'/js/jquery.lazyloadxt.min.js', array( 'jquery' ), null, true);
-	wp_enqueue_script( 'lazyloadxt-srcset', $assetsDir.'/js/jquery.lazyloadxt.srcset.min.js', array( 'jquery' ), null, true);
-	wp_enqueue_script( 'app', $assetsDir.'/js/app.js', array( 'jquery', 'lazyloadxt' ), null, true);
-	wp_enqueue_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js', array( 'jquery' ), null, true);
+	wp_enqueue_script( 'jquery', '//code.jquery.com/jquery-latest.min.js', null, null, true );
+	if ( strpos($_SERVER['SERVER_NAME'], 'responsify-demo.dev') ) {
+		if ( !empty($_GET['production']) ) {
+			wp_enqueue_script( 'scripts', $assetsDir.'/js/scripts.min.js', array( 'jquery' ), filemtime($assetsPath.'/js/scripts.min.js'), true);
+		} else {
+			wp_enqueue_script( 'lazyloadxt', $assetsDir.'/js/jquery.lazyloadxt.min.js', array( 'jquery' ), null, true);
+			wp_enqueue_script( 'lazyloadxt-srcset', $assetsDir.'/js/jquery.lazyloadxt.srcset.min.js', array( 'jquery' ), null, true);
+			wp_enqueue_script( 'app', $assetsDir.'/js/app.js', array( 'jquery', 'lazyloadxt' ), null, true);
+		}
+	} else {
+		wp_enqueue_script( 'scripts', $assetsDir.'/js/scripts.min.js', array( 'jquery' ), filemtime($assetsPath.'/js/scripts.min.js'), true);
+	}
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_scripts_and_styles' );
